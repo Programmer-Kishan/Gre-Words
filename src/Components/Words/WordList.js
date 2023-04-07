808bf0112d67928ba7b24d835b193b1dddcf0296
unknown <kishankathare45@gmail.com>
2023-04-07 21:31:56 +0530

    Added 10 words

---
+++ src/Components/Words/WordList.js
@@ -0,0 +1,43 @@
+
+const wordlist = [
+    {
+        'word': 'Tyro',
+        'meaning': 'A person who is new to an activity'
+    },
+    {
+        'word': 'prodigy',
+        'meaning': 'someone wiht very great ability which shows itself when the person is young'
+    },
+    {
+        'word': 'recluse',
+        'meaning': 'A person who lives alone and aviods going outside and talking to other people'
+    },
+    {
+        'word': 'outcast',
+        'meaning': 'A person who has no place in the society or group, because the society or group refuses to accept them'
+    },
+    {
+        'word': 'prodigal',
+        'meaning': 'spending a large amount of time, money and energy in a way that is now very nice'
+    },
+    {
+        'word': 'pedagogic',
+        'meaning': 'relating to the practice of teaching and its method'
+    },
+    {
+        'word': 'oversee',
+        'meaning': 'to watch or orgainize a job or activity to certain that it is being done correctly'
+    },
+    {
+        'word': 'vindicate',
+        'meaning': 'to prove that what someone did or said was right or true, after other people thought it was wrong'
+    },
+    {
+        'word': 'clandestine',
+        'meaning': 'planned or done in secret, especially describing something that is not offically allowed'
+    },
+    {
+        'word': 'sanguine',
+        'meaning': '(of someone or someones character) positive and hoping for good things or hopeful'
+    },
+]
